f86d2d86bdebf4b2b1501cdb67e46b010e284127
Sebastian Buczyński <nnplaya@gmail.com>
2022-07-30 23:20:09 +0200

    Add leaven for correlation and causation ids

--- event_sourcery/event.py
+++ event_sourcery/event.py
@@ -1,8 +1,18 @@
 from datetime import datetime
-from typing import Protocol
+from typing import Optional, Protocol
 from uuid import UUID
+
+
+class Metadata(Protocol):
+    correlation_id: Optional[UUID]
+    causation_id: Optional[UUID]
 
 
 class Event(Protocol):
     uuid: UUID
     created_at: datetime
+
+    @property
+    def metadata(self) -> Metadata:
+        # https://mypy.readthedocs.io/en/latest/common_issues.html#covariant-subtyping-of-mutable-protocol-members-is-rejected
+        ...
--- event_sourcery_pydantic/event.py
+++ event_sourcery_pydantic/event.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import Any, ClassVar
+from typing import Any, ClassVar, Optional
 from uuid import UUID, uuid4
 
 from pydantic import BaseModel, Field
@@ -7,9 +7,16 @@ from pydantic import BaseModel, Field
 from event_sourcery.event_registry import EventRegistry
 
 
+class Metadata(BaseModel):
+    # TODO: whether model this as a dict, or allow extra fields via pydantic config
+    correlation_id: Optional[UUID]
+    causation_id: Optional[UUID]
+
+
 class Event(BaseModel):
     uuid: UUID = Field(default_factory=uuid4)
     created_at: datetime = Field(default_factory=datetime.utcnow)
+    metadata: Metadata = Field(default_factory=Metadata)
 
     __registry__: ClassVar = EventRegistry()
 
