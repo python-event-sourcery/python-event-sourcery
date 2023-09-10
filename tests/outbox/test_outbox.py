afec639d40fcacf06001789bafbe821afa183390
Lukasz Zukowski <lukasz.zukowski@affirm.com>
2023-09-10 12:21:35 +0200

    Refactor: move TestFiltererWhichFiltersOutEverything to separate test module

--- tests/outbox/test_outbox.py
+++ tests/outbox/test_outbox.py
@@ -1,12 +1,8 @@
 from unittest.mock import Mock
 from uuid import uuid4
 
-import pytest
-
 from event_sourcery import Metadata, StreamId
 from event_sourcery.event_store import EventStore
-from event_sourcery.factory import EventStoreFactory
-from event_sourcery.interfaces.outbox_filterer_strategy import OutboxFiltererStrategy
 from event_sourcery.outbox import Outbox
 from tests.events import SomeEvent
 
@@ -59,24 +55,3 @@ def test_tries_to_send_up_to_three_times(
         outbox.run_once()
 
     assert len(publisher.mock_calls) == 3
-
-
-class TestFiltererWhichFiltersOutEverything:
-    @pytest.fixture()
-    def filterer(self) -> OutboxFiltererStrategy:
-        return lambda event: False
-
-    @pytest.fixture()
-    def event_store(
-        self, filterer: OutboxFiltererStrategy, event_store_factory: EventStoreFactory
-    ) -> EventStore:
-        return event_store_factory.with_outbox(filterer=filterer).build()
-
-    def test_no_entries_are_published(
-        self, outbox: Outbox, publisher: Mock, event_store: EventStore
-    ) -> None:
-        an_event = Metadata[SomeEvent](event=SomeEvent(first_name="John"), version=1)
-        event_store.publish(an_event, stream_id=StreamId(uuid4()))
-        outbox.run_once()
-
-        assert len(publisher.mock_calls) == 0
