b5e98f6522ce05203eaa550052c8b6541609812b
Lukasz Zukowski <lukasz.zukowski@affirm.com>
2023-09-10 13:53:54 +0200

    Refactor: EventStoreFactory

--- event_sourcery_esdb/__init__.py
+++ event_sourcery_esdb/__init__.py
@@ -6,7 +6,6 @@ __all__ = [
 from esdbclient import EventStoreDBClient
 from typing_extensions import Self
 
-from event_sourcery import EventStore
 from event_sourcery.dummy_outbox_filterer_strategy import dummy_filterer
 from event_sourcery.factory import EventStoreFactory
 from event_sourcery.interfaces.outbox_filterer_strategy import OutboxFiltererStrategy
@@ -16,13 +15,9 @@ from event_sourcery_esdb.outbox import ESDBOutboxStorageStrategy
 
 class ESDBStoreFactory(EventStoreFactory):
     def __init__(self, esdb: EventStoreDBClient) -> None:
-        super().__init__()
         self._client = esdb
-        self._storage_strategy = ESDBStorageStrategy(self._client)
+        self._configure(storage_strategy=ESDBStorageStrategy(self._client))
 
     def with_outbox(self, filterer: OutboxFiltererStrategy = dummy_filterer) -> Self:
-        self._outbox_strategy = ESDBOutboxStorageStrategy(self._client, filterer)
-        return self
-
-    def build(self) -> EventStore:
-        return super().build()
+        strategy = ESDBOutboxStorageStrategy(self._client, filterer)
+        return self._configure(outbox_storage_strategy=strategy)
--- event_sourcery_sqlalchemy/__init__.py
+++ event_sourcery_sqlalchemy/__init__.py
@@ -8,7 +8,6 @@ __all__ = [
 from sqlalchemy.orm import Session
 from typing_extensions import Self
 
-from event_sourcery import EventStore
 from event_sourcery.dummy_outbox_filterer_strategy import dummy_filterer
 from event_sourcery.factory import EventStoreFactory
 from event_sourcery.interfaces.outbox_filterer_strategy import OutboxFiltererStrategy
@@ -20,13 +19,9 @@ from event_sourcery_sqlalchemy.sqlalchemy_outbox import SqlAlchemyOutboxStorageS
 
 class SQLStoreFactory(EventStoreFactory):
     def __init__(self, session: Session) -> None:
-        super().__init__()
         self._session = session
-        self._storage_strategy = SqlAlchemyStorageStrategy(session)
+        self._configure(storage_strategy=SqlAlchemyStorageStrategy(self._session))
 
     def with_outbox(self, filterer: OutboxFiltererStrategy = dummy_filterer) -> Self:
-        self._outbox_strategy = SqlAlchemyOutboxStorageStrategy(self._session, filterer)
-        return self
-
-    def build(self) -> EventStore:
-        return super().build()
+        strategy = SqlAlchemyOutboxStorageStrategy(self._session, filterer)
+        return self._configure(outbox_storage_strategy=strategy)
