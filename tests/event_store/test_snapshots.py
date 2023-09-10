52948d51ca977b5a5ba117a29f04717a6d5fc514
Lukasz Zukowski <lukasz.zukowski@affirm.com>
2023-09-10 20:41:06 +0200

    Tests: Snapshot multiple times

--- tests/event_store/test_snapshots.py
+++ tests/event_store/test_snapshots.py
@@ -2,7 +2,10 @@ from uuid import uuid4
 
 from event_sourcery import Event, Metadata, StreamId
 from event_sourcery.event_store import EventStore
-from tests.events import SomeEvent
+
+
+class AnEvent(Event):
+    pass
 
 
 class Snapshot(Event):
@@ -11,10 +14,41 @@ class Snapshot(Event):
 
 def test_handles_snapshots(event_store: EventStore) -> None:
     stream_id = StreamId(uuid4())
-    event = Metadata[SomeEvent](event=SomeEvent(first_name="Test"), version=1)
-    event_store.append(event, stream_id=stream_id)
-    snapshot = Metadata[Snapshot](event=Snapshot(), version=1)
+    event_store.append(Metadata.wrap(event=AnEvent(), version=1), stream_id=stream_id)
+    snapshot = Metadata.wrap(event=Snapshot(), version=1)
     event_store.save_snapshot(stream_id=stream_id, snapshot=snapshot)
 
     events = event_store.load_stream(stream_id=stream_id)
     assert events == [snapshot]
+
+
+def test_handles_multiple_snapshots(event_store: EventStore) -> None:
+    stream_id = StreamId()
+    event_store.append(Metadata.wrap(event=AnEvent(), version=1), stream_id=stream_id)
+    event_store.save_snapshot(
+        stream_id=stream_id, snapshot=Metadata.wrap(event=Snapshot(), version=1)
+    )
+    event_store.append(Metadata.wrap(event=AnEvent(), version=2), stream_id=stream_id)
+
+    last = Metadata.wrap(event=Snapshot(), version=2)
+    event_store.save_snapshot(stream_id=stream_id, snapshot=last)
+
+    events = event_store.load_stream(stream_id=stream_id)
+    assert events == [last]
+
+
+def test_returns_all_events_after_last_snapshot(event_store: EventStore) -> None:
+    stream_id = StreamId()
+    event_store.append(Metadata.wrap(event=AnEvent(), version=1), stream_id=stream_id)
+    last_snapshot = Metadata.wrap(event=Snapshot(), version=1)
+    event_store.save_snapshot(stream_id=stream_id, snapshot=last_snapshot)
+
+    additional_events = [
+        Metadata.wrap(event=AnEvent(), version=2),
+        Metadata.wrap(event=AnEvent(), version=3),
+        Metadata.wrap(event=AnEvent(), version=4),
+    ]
+    event_store.append(*additional_events, stream_id=stream_id)
+
+    events = event_store.load_stream(stream_id=stream_id)
+    assert events == [last_snapshot] + additional_events
