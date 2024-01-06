d60ff1c7c45f21f2a736b2924ee54f320ebb706f
Łukasz Żukowski <lzukowski@rippling.com>
2024-01-06 15:25:24 +0100

    Tests: lacking scenario for deleting not existing stream

--- tests/event_store/test_removing_stream.py
+++ tests/event_store/test_removing_stream.py
@@ -8,3 +8,8 @@ def test_removes_stream(given: Given, then: Then, when: When) -> None:
     given.event(AnEvent(), on=stream)
     when.deletes(stream)
     then.stream(stream).is_empty()
+
+
+def test_nothing_when_removing_not_existing_stream(then: Then, when: When) -> None:
+    when.deletes(stream := StreamId(name="Not existing"))
+    then.stream(stream).is_empty()
