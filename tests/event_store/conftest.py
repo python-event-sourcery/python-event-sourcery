ed09c6af2248fd8f75b74aad9627be116848fae6
Sebastian Buczyński <nnplaya@gmail.com>
2024-04-27 22:25:30 +0200

    Implement tests for transactional stores and handling

--- tests/event_store/conftest.py
+++ tests/event_store/conftest.py
@@ -1,14 +1,18 @@
-from typing import cast
+from contextlib import nullcontext
+from typing import Callable, ContextManager, Iterator, cast
 
 import pytest
 from _pytest.fixtures import SubRequest
+from django.db import transaction as django_transaction
 
 from event_sourcery.event_store import BackendFactory
+from event_sourcery_django import DjangoBackendFactory
 from tests import mark
 from tests.backend.django import django
 from tests.backend.esdb import esdb
 from tests.backend.in_memory import in_memory
 from tests.backend.sqlalchemy import sqlalchemy_postgres, sqlalchemy_sqlite
+from tests.event_store.other_client import OtherClient
 
 
 @pytest.fixture(
@@ -25,3 +29,17 @@ def event_store_factory(request: SubRequest) -> BackendFactory:
     mark.xfail_if_not_implemented_yet(request, backend_name)
     mark.skip_backend(request, backend_name)
     return cast(BackendFactory, request.getfixturevalue(backend_name))
+
+
+@pytest.fixture
+def other_client(event_store_factory: BackendFactory) -> Iterator[OtherClient]:
+    transaction_ctx: Callable[[], ContextManager]
+    if isinstance(event_store_factory, DjangoBackendFactory):
+        transaction_ctx = django_transaction.atomic
+    else:
+        # TODO: SQLALchemy should pass session.begin() or equivalent
+        transaction_ctx = nullcontext
+
+    client = OtherClient(event_store_factory, transaction_ctx)
+    yield client
+    client.stop()
--- tests/event_store/test_subscription.py
+++ tests/event_store/test_subscription.py
@@ -7,6 +7,7 @@ import pytest
 from event_sourcery.event_store import Entry, Event, EventStore, StreamId
 from event_sourcery.event_store.factory import Backend
 from tests.bdd import Given, Subscription, Then, When
+from tests.event_store.other_client import OtherClient
 from tests.factories import an_event
 from tests.matchers import any_record
 
@@ -417,6 +418,68 @@ class TestSubscribeToEventTypes:
         then(subscription).next_batch_is([any_record(third), any_record(fourth)])
         then(subscription).next_batch_is([any_record(fifth)])
         then(subscription).next_batch_is_empty()
+
+
+@pytest.mark.skip_backend(
+    backend=["esdb", "in_memory"],
+    reason="Required only for SQL-based backends with transactions",
+)
+class TestHandlingUncommittedMessagesFromOtherClients:
+    @pytest.mark.not_implemented(
+        backend=["sqlalchemy_sqlite", "sqlalchemy_postgres"],
+    )
+    def test_ignores_events_from_pending_transaction(
+        self,
+        given: Given,
+        then: Then,
+        other_client: OtherClient,
+    ) -> None:
+        subscription = given.subscription(timelimit=1)
+
+        other_client.appends_in_transaction(an_event(version=1), StreamId())
+
+        then(subscription).received_no_new_records()
+
+    @pytest.mark.not_implemented(
+        backend=["sqlalchemy_sqlite", "sqlalchemy_postgres"],
+    )
+    def test_gets_events_after_transaction_gets_committed(
+        self,
+        given: Given,
+        then: Then,
+        other_client: OtherClient,
+    ) -> None:
+        subscription = given.subscription(timelimit=1)
+
+        other_transaction = other_client.appends_in_transaction(
+            event := an_event(version=1), stream := StreamId()
+        )
+
+        then(subscription).received_no_new_records()
+        other_transaction.commit()
+        then(subscription).next_received_record_is(any_record(event, stream))
+
+    @pytest.mark.not_implemented(
+        backend=["sqlalchemy_sqlite", "sqlalchemy_postgres"],
+    )
+    def test_misses_event_that_was_not_committed_while_got_newer_one(
+        self,
+        given: Given,
+        then: Then,
+        when: When,
+        other_client: OtherClient,
+    ) -> None:
+        subscription = given.subscription(timelimit=1)
+
+        other_transaction = other_client.appends_in_transaction(
+            an_event(version=1), StreamId()
+        )
+        other_transaction.process_up_to_commit()
+        stream = when.stream().receives(event := an_event())
+
+        then(subscription).next_received_record_is(any_record(event, stream.id))
+        other_transaction.commit()
+        then(subscription).received_no_new_records()
 
 
 @pytest.mark.skip_backend(backend="esdb", reason="ESDB don't have transactions")
