ed89f7195a9aef017653bb93e61e638c05c81a75
Łukasz Żukowski <lukasz.zukowski@gmail.com>
2024-04-28 11:38:52 +0200

    Fix: FastAPI example

--- examples/0-fastapi-integration/api.py
+++ examples/0-fastapi-integration/api.py
@@ -1,12 +1,16 @@
 from typing import Iterator, Literal
-from uuid import uuid4
 
 from fastapi import FastAPI, Depends
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData, StaticPool
 from sqlalchemy.orm import Session, as_declarative
+from starlette.testclient import TestClient
 
 # Import a couple of things from event_sourcery
-from event_sourcery.event_store import Event, EventStore  # base class for events
+from event_sourcery.event_store import (
+    Backend,
+    Event,  # base class for events
+    StreamId,
+)
 from event_sourcery_sqlalchemy import (
     configure_models,  # set-up function for library's models
     SQLAlchemyBackendFactory # sql store factory to start quickly
@@ -14,8 +18,15 @@ from event_sourcery_sqlalchemy import (
 
 # Set up your DB and application as you would normally do
 engine = create_engine(
-    "postgresql://event_sourcery:event_sourcery@localhost:5432/event_sourcery"
+    "sqlite:///:memory:",
+    connect_args={'check_same_thread': False},
+    poolclass=StaticPool,
 )
+
+
+@as_declarative()
+class Base:
+    metadata: MetaData
 
 
 def db_session() -> Iterator[Session]:
@@ -26,20 +37,17 @@ def db_session() -> Iterator[Session]:
         session.close()
 
 
-@as_declarative()
-class Base:
-    pass
-
-
-app = FastAPI(on_startup=[lambda: Base.metadata.create_all(bind=engine)])
-
-
 # initialize Event Sourcery models, so they can be handled by SQLAlchemy and e.g. alembic
-configure_models(Base)
+def register_db() -> None:
+    configure_models(Base)
+    Base.metadata.create_all(bind=engine)
+
+
+app = FastAPI(on_startup=[register_db])
 
 
 # Set up factory for event store to be injected into views
-def event_store(session: Session = Depends(db_session)) -> EventStore:
+def backend(session: Session = Depends(db_session)) -> Backend:
     return SQLAlchemyBackendFactory(session).build()
 
 
@@ -51,11 +59,22 @@ class CustomerSubscribed(Event):
 
 @app.post("/subscription")
 def subscribe(
-    event_store: EventStore = Depends(event_store),
+    pyes: Backend = Depends(backend),
     session: Session = Depends(db_session),
 ):
     # Create an event
     event = CustomerSubscribed(plan_id=1, model="yearly")
     # Put it in the event store!
-    event_store.append(event, stream_id=uuid4())
+    pyes.event_store.append(event, stream_id=StreamId())
     session.commit()
+
+
+def test_post():
+    with TestClient(app) as client:
+        result = client.post("/subscription")
+    assert result.status_code == 200
+
+
+if __name__ == "__main__":
+    from pytest import main
+    main()
