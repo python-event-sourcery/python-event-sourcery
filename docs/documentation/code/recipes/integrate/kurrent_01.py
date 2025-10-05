16bb76b6fa90f7b66d3d6d96457114780b2b447b
Łukasz Żukowski <lukasz.zukowski@gmail.com>
2025-10-05 12:26:44 +0200

    Fix: docs

--- docs/documentation/code/recipes/integrate/kurrent_01.py
+++ docs/documentation/code/recipes/integrate/kurrent_01.py
@@ -4,5 +4,5 @@ from event_sourcery.event_store import StreamId
 from event_sourcery_kurrentdb import KurrentDBBackend
 
 client = KurrentDBClient(uri="kurrentdb://localhost:2113?Tls=false")
-backend = KurrentDBBackend(client)
+backend = KurrentDBBackend().configure(client)
 backend.event_store.load_stream(StreamId())  # test if connection works
